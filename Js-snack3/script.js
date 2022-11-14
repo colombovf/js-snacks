67b65cfd7a9def0563c9377b876f05dc47672844
colombovf <colombovf@gmail.com>
2022-11-14 15:44:04 +0100

    Js-snack3 sono riuscito a chiedere i 10 input nel for loop e mettere i risultati nell'array

--- Js-snack3/script.js
+++ Js-snack3/script.js
@@ -1,7 +1,9 @@
-//Array = ["numero_1","numero_2","numero_3","numero_4","numero_5","numero_6","numero_7","numero_8","numero_9","numero_10"]
+numeri_immessi_salvati = []
 
 for (i = 0; i < 10; i ++) {
-   let numeri_immessi =  +prompt("Inserisìre un numero:");
-    console.log(numeri_immessi);
-}
-
+    let numeri_input = +prompt("Inserire un numero:")
+    numeri_immessi_salvati.push(numeri_input);
+    
+    }
+    
+console.log(numeri_immessi_salvati)
