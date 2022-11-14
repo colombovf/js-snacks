34e67bd9050575ebc816c9ecfb484fbac62a3a9c
colombovf <colombovf@gmail.com>
2022-11-14 14:10:17 +0100

    js-snack1 Fine esercizio

---
+++ Js-snack1/script.js
@@ -0,0 +1,10 @@
+let numero_1 = prompt("Inserire un numero:");
+let numero_2 = prompt("Inserire un'altro numero:");
+let numero_maggiore;
+
+
+if (numero_1 >= numero_2) {
+    console.log("Il numero maggiore è: " + numero_1)
+} else if (numero_2 >= numero_1) {
+    console.log("Il numero maggiore è: " +numero_2)
+}
