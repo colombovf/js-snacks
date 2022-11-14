491adb446cb038c21d8080fc9b05b9512cf86f72
colombovf <colombovf@gmail.com>
2022-11-14 15:02:03 +0100

    js-snack3 esercizio in corso

---
+++ Js-snack3/script.js
@@ -0,0 +1,7 @@
+//Array = ["numero_1","numero_2","numero_3","numero_4","numero_5","numero_6","numero_7","numero_8","numero_9","numero_10"]
+
+for (i = 0; i < 10; i ++) {
+   let numeri_immessi =  +prompt("Inserisìre un numero:");
+    console.log(numeri_immessi);
+}
+
